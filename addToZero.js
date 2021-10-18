600b50cf663749d67a3e2ec3585975c61417e46c
Cameron Robinson <92333068+camrobinson6@users.noreply.github.com>
2021-10-18 09:56:10 -0500

    Add files via upload
    updated code

--- addToZero.js
+++ addToZero.js
@@ -1,20 +1,23 @@
 // Starting array
+//let array = [1, 4, 11, 2, 37, -4]
+//let array = [0, 21, 33, 6, 0, -9]
+//let array = [0, 1, 2, 3, 4, 5]
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 
 // Write your solution below:
-let value = 'False'
+let sum = 'False'
 
-for (i = 0; i < array.length-1; i++){
-for (j = 0; j < array.length-1; j++){
+for (i = 0; i < array.length; i++){
+for (j = 0; j < array.length; j++){
     if (i !== j) {
         if (array[i] + array[j] === 0) {
-          value = "True"
+          sum= "True"
         } 
       }
     }
   }
   
-  console.log(value)
+  console.log(sum)
 
-//Code ran sucessfulluy
+//Code ran sucessfully

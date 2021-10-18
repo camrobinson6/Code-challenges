1ba38a9e9c997158eaf28e58780808183e1357b8
Cameron Robinson <92333068+camrobinson6@users.noreply.github.com>
2021-10-18 08:25:28 -0500

    Add files via upload

---
+++ addToZero.js
@@ -0,0 +1,10 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, 12]
+let t = true
+let f = false
+// Write your solution below:
+if (array === 0){
+    console.log(true)
+} else {
+    console.log(false)
+}

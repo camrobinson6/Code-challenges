ffb7a221eb8ab158290f85605e8f32cdc2a7686c
Cameron Robinson <92333068+camrobinson6@users.noreply.github.com>
2021-10-18 09:27:42 -0500

    Add files via upload

--- addToZero.js
+++ addToZero.js
@@ -1,10 +1,20 @@
 // Starting array
 let array = [28, 43, -12, 30, 4, 0, 12]
-let t = true
-let f = false
+
+
 // Write your solution below:
-if (array === 0){
-    console.log(true)
-} else {
-    console.log(false)
-}
+let value = 'False'
+
+for (i = 0; i < array.length; i++){
+for (j = 0; j < array.length; j++){
+    if (i !== j) {
+        if (array[i] + array[j] === 0) {
+          value = "True"
+        } 
+      }
+    }
+  }
+  
+  console.log(value)
+
+//Code ran sucessfulluy

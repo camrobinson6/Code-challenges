0bab80ff03e8b3008ee0e93103ea80dca9ba2f04
Cameron Robinson <92333068+camrobinson6@users.noreply.github.com>
2021-10-18 09:41:39 -0500

    Add files via upload

--- addToZero.js
+++ addToZero.js
@@ -5,8 +5,8 @@ let array = [28, 43, -12, 30, 4, 0, 12]
 // Write your solution below:
 let value = 'False'
 
-for (i = 0; i < array.length; i++){
-for (j = 0; j < array.length; j++){
+for (i = 0; i < array.length-1; i++){
+for (j = 0; j < array.length-1; j++){
     if (i !== j) {
         if (array[i] + array[j] === 0) {
           value = "True"

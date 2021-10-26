76b51ac2eb0b98e329e8670726cb2ce8364adb87
Cameron Robinson <92333068+camrobinson6@users.noreply.github.com>
2021-10-26 08:16:42 -0500

    Add files via upload

--- hasMoreVowels.js
+++ hasMoreVowels.js
@@ -10,19 +10,3 @@ return true
 } 
 
 
-//   let vowelsNeeded = word.length / 2
-//   let wordSplit = word.toLowerCase().split('')
-
-//   for (let i = 0; i < wordSplit.length; i++) {
-//     let letter = wordSplit[i]
-
-//     if (vowels.includes(letter)) {
-//       vowelCount++
-//     }
-
-//     if (vowelCount > vowelsNeeded) {
-//       return true
-//     }
-//   }
-
-//   return false

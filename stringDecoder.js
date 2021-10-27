7db505be77f6887457870197d521d5a49cb0a5a7
Cameron Robinson <92333068+camrobinson6@users.noreply.github.com>
2021-10-27 09:26:34 -0500

    Add files via upload

---
+++ stringDecoder.js
@@ -0,0 +1,17 @@
+const decoder = code =>{
+        let result = '';
+        let i = 0, n = code.length;
+        while (i < n) {
+            if (Number(code[i]) == code[i]) {
+                i += parseInt(code[i]) + 1;
+            }
+            else
+                result += code[i++];
+        }
+        return result;
+    }
+let result = decoder('2bna0p1mp2osl0e');
+console.log(result);
+
+
+
